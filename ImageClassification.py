bb4ba1edd36c9943751090fc7ed55881a39569a1
ZackBudai <64515138+ZackBudai@users.noreply.github.com>
2022-12-22 18:26:37 +1000

    Add files via upload

---
+++ ImageClassification.py
@@ -0,0 +1,112 @@
+## Author: Zack Budai
+## Date 22 Dec 2022
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+# Set up the device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load the MNIST dataset
+train_dataset = torchvision.datasets.MNIST(
+    root='.',
+    train=True,
+    transform=transforms.ToTensor(),
+    download=True
+)
+
+test_dataset = torchvision.datasets.MNIST(
+    root='.',
+    train=False,
+    transform=transforms.ToTensor()
+)
+
+# Create data loaders
+batch_size = 64
+train_loader = torch.utils.data.DataLoader(
+    train_dataset,
+    batch_size=batch_size,
+    shuffle=True
+)
+
+test_loader = torch.utils.data.DataLoader(
+    test_dataset,
+    batch_size=batch_size,
+    shuffle=False
+)
+
+# Define the model
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.ReLU(x)
+        x = self.conv2(x)
+        x = nn.ReLU(x)
+        x = nn.MaxPool2d(2)(x)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = nn.ReLU(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = nn.LogSoftmax(dim=1)(x)
+        return output
+
+# Create the model and move it to the device
+model = Net().to(device)
+
+# Define the loss function and the optimizer
+criterion = nn
+
+# Train the model
+n_epochs = 10
+for epoch in range(n_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # Move the data to the device
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Zero the gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward pass and optimization step
+        loss.backward()
+        optimizer.step()
+
+        # Print the progress
+        if (i+1) % 100 == 0:
+            print(f'Epoch [{epoch+1}/{n_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+
+# Test the model
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        # Move the data to the device
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print(f'Accuracy of the model on the test set: {100 * correct / total} %')
